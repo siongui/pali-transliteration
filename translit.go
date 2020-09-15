ed0db30de6c4eabf29961660b33bc77232307f12
siongui <siongui@gmail.com>
2020-09-15 23:38:52 +0800

    add RomanToThai. not sure if the conversion is correct or not

--- translit.go
+++ translit.go
@@ -1,5 +1,172 @@
 package palitrans
 
+import (
+	"strings"
+)
+
+// RomanToThai converts Romanized Pāli to Thai script Pāli.
+func RomanToThai(rm string) string {
+
+	input := strings.Replace(strings.ToLower(rm), "ṁ", "ṃ", -1)
+
+	vowel := make(map[string]string)
+	vowel["a"] = "1"
+	vowel["ā"] = "1"
+	vowel["i"] = "1"
+	vowel["ī"] = "1"
+	vowel["iṃ"] = "1"
+	vowel["u"] = "1"
+	vowel["ū"] = "1"
+	vowel["e"] = "2"
+	vowel["o"] = "2"
+
+	thair := make(map[string]string)
+	thair["a"] = "อ"
+	thair["ā"] = "า"
+	thair["i"] = "\u0e34"
+	thair["ī"] = "\u0e35"
+	thair["iṃ"] = "\u0e36"
+	thair["u"] = "\u0e38"
+	thair["ū"] = "\u0e39"
+	thair["e"] = "เ"
+	thair["o"] = "โ"
+	thair["ṃ"] = "\u0e4d"
+	thair["k"] = "ก"
+	thair["kh"] = "ข"
+	thair["g"] = "ค"
+	thair["gh"] = "ฆ"
+	thair["ṅ"] = "ง"
+	thair["c"] = "จ"
+	thair["ch"] = "ฉ"
+	thair["j"] = "ช"
+	thair["jh"] = "ฌ"
+	thair["ñ"] = "ญ"
+	thair["ṭ"] = "ฏ"
+	thair["ṭh"] = "ฐ"
+	thair["ḍ"] = "ฑ"
+	thair["ḍh"] = "ฒ"
+	thair["ṇ"] = "ณ"
+	thair["t"] = "ต"
+	thair["th"] = "ถ"
+	thair["d"] = "ท"
+	thair["dh"] = "ธ"
+	thair["n"] = "น"
+	thair["p"] = "ป"
+	thair["ph"] = "ผ"
+	thair["b"] = "พ"
+	thair["bh"] = "ภ"
+	thair["m"] = "ม"
+	thair["y"] = "ย"
+	thair["r"] = "ร"
+	thair["l"] = "ล"
+	thair["ḷ"] = "ล"
+	thair["v"] = "ว"
+	thair["s"] = "ส"
+	thair["h"] = "ห"
+
+	cons := make(map[string]string)
+	cons["k"] = "1"
+	cons["g"] = "1"
+	cons["ṅ"] = "1"
+	cons["c"] = "1"
+	cons["j"] = "1"
+	cons["ñ"] = "1"
+	cons["ṭ"] = "1"
+	cons["ḍ"] = "1"
+	cons["ṇ"] = "1"
+	cons["t"] = "1"
+	cons["d"] = "1"
+	cons["n"] = "1"
+	cons["p"] = "1"
+	cons["b"] = "1"
+	cons["m"] = "1"
+	cons["y"] = "1"
+	cons["r"] = "1"
+	cons["l"] = "1"
+	cons["ḷ"] = "1"
+	cons["v"] = "1"
+	cons["s"] = "1"
+	cons["h"] = "1"
+
+	var i0 = ""
+	var i1 = ""
+	var i2 = ""
+	var i3 = ""
+	var output = ""
+	var i = 0
+
+	input = strings.Replace(input, "&quot;", "`", -1)
+
+	for i < length(input) {
+		im := CharAt(input, i-2)
+		i0 = CharAt(input, i-1)
+		i1 = CharAt(input, i)
+		i2 = CharAt(input, i+1)
+		i3 = CharAt(input, i+2)
+
+		if _, ok := vowel[i1]; ok {
+			if i1 == "o" || i1 == "e" {
+				output += thair[i1] + thair["a"]
+				i++
+			} else {
+				if i == 0 {
+					output += thair["a"]
+				}
+				if i1 == "i" && i2 == "ṃ" { // special i.m character
+					output += thair[i1+i2]
+					i++
+				} else if i1 != "a" {
+					output += thair[i1]
+				}
+				i++
+			}
+		} else if _, ok2 := thair[i1+i2]; ok2 && i2 == "h" { // two character match
+			if i3 == "o" || i3 == "e" {
+				output += thair[i3]
+				i++
+			}
+			output += thair[i1+i2]
+			if _, ok3 := cons[i3]; ok3 {
+				output += "\u0e3a"
+			}
+			i = i + 2
+		} else if _, ok4 := thair[i1]; ok4 && i1 != "a" { // one character match except a
+			if i2 == "o" || i2 == "e" {
+				output += thair[i2]
+				i++
+			}
+			output += thair[i1]
+			if _, ok5 := cons[i2]; ok5 && i1 != "ṃ" {
+				output += "\u0e3a"
+			}
+			i++
+		} else if _, ok6 := thair[i1]; !ok6 {
+			output += i1
+			_, ok7 := cons[i0]
+
+			if _, ok71 := cons[im]; ok7 || (i0 == "h" && ok71) {
+				output += "\u0e3a"
+			}
+
+			i++
+			if i2 == "o" || i2 == "e" { // long vowel first
+				output += thair[i2]
+				i++
+			}
+			if _, ok8 := vowel[i2]; ok8 { // word-beginning vowel marker
+				output += thair["a"]
+			}
+		} else { // a
+			i++
+		}
+	}
+	if _, ok9 := cons[i1]; ok9 {
+		output += "\u0e3a"
+	}
+	//js: output = output.replace(/\`+/g, '"');
+	return output
+}
+
 // CharAt is Go implementation of JavaScript charAt function.
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
 func CharAt(str string, index int) string {
@@ -16,4 +183,12 @@ func CharAt(str string, index int) string {
 	}
 
 	return ""
+}
+
+func length(str string) int {
+	i := 0
+	for range str {
+		i++
+	}
+	return i
 }
--- translit_test.go
+++ translit_test.go
@@ -4,6 +4,57 @@ import (
 	"testing"
 )
 
+func TestRomanToThai(t *testing.T) {
+	var mapping = map[string]string{
+		"ปา":   "pā",
+		"ลิ":   "li",
+		"ลี":   "lī",
+		"ปาลิ": "pāli",
+		"สาธุ": "sādhu",
+		"ก":    "ka",
+		"กิ":   "ki",
+		"กุ":   "ku",
+		"กา":   "kā",
+		"กี":   "kī",
+		"กู":   "kū",
+		//"เก":   "ke",
+		//"โก":   "ko",
+		"กจ":      "kaca",
+		"อิติ":    "iti",
+		"เอว":     "eva",
+		"อปิ":     "api",
+		"อาชิ":    "āji",
+		"อุป":     "upa",
+		"กํ":      "kaṃ",
+		"นรํ":     "naraṃ",
+		"ภิกฺขุ":  "bhikkhu",
+		"ทุกฺข":   "dukkha",
+		"พุทฺเธน": "buddhena",
+		"อหํ":     "ahaṃ",
+		"พุทฺธํ":  "buddhaṃ",
+		//"สรณํ":    "saranaṃ",
+		"คจฺฉามิ": "gacchāmi",
+		//"นโม":     "namo",
+		"ทสฺส": "dassa",
+		//"ภควโต":   "bhagavato",
+		"ธมฺมานิ":   "dhammāni",
+		"คนฺตฺวา":   "gantvā",
+		"มฺหิ":      "mhi",
+		"ยฺห":       "yha",
+		"มยฺหํ":     "mayhaṃ",
+		"อคฺคิมฺหิ": "aggimhi",
+		"กึ":        "kiṃ",
+		"":          "",
+	}
+
+	for k, v := range mapping {
+		if k != RomanToThai(v) {
+			t.Error(k, v)
+			return
+		}
+	}
+}
+
 func TestCharAt(t *testing.T) {
 	s := CharAt("abc", -1)
 	if s != "" {
@@ -38,6 +89,23 @@ func TestCharAt(t *testing.T) {
 	s = CharAt("ādā", 3)
 	if s != "" {
 		t.Error(s)
+		return
+	}
+}
+
+func TestLength(t *testing.T) {
+	if length("I am 字串") != 7 {
+		t.Error("I am 字串")
+		return
+	}
+
+	if length("ādā") != 3 {
+		t.Error("ādā")
+		return
+	}
+
+	if length("abc") != 3 {
+		t.Error("abc")
 		return
 	}
 }
